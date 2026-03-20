449e4905cef44822696a9420d2756fd63cdaef0f
GriseldaTNHutahaean <iss24026@students.del.ac.id>
2026-03-20 15:33:55 +0700

    Update Enrollment.java

--- src/academic/model/Enrollment.java
+++ src/academic/model/Enrollment.java
@@ -1,11 +1,43 @@
 package academic.model;
 
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S24026 Griselda Hutahaean
  */
 public class Enrollment {
 
-    // class definition
+    private String courseCode;
+    private String studentNim;
+    private String academicYear;
+    private String semester;
+    private String grade;
 
+    // constructor (TANPA input grade)
+    public Enrollment(String courseCode, String studentNim, String academicYear, String semester) {
+        this.courseCode = courseCode;
+        this.studentNim = studentNim;
+        this.academicYear = academicYear;
+        this.semester = semester;
+        this.grade = "None"; // default sesuai soal
+    }
+
+    // getter
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public String getStudentNim() {
+        return studentNim;
+    }
+
+    public String getAcademicYear() {
+        return academicYear;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
 }

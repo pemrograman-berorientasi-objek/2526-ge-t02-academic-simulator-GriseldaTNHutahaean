8d74322843f8331d0495c80c7cc6bd7e652a4f27
GriseldaTNHutahaean <iss24026@students.del.ac.id>
2026-03-20 15:33:40 +0700

    Update Course.java

--- src/academic/model/Course.java
+++ src/academic/model/Course.java
@@ -1,11 +1,37 @@
 package academic.model;
 
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S24026 Griselda Hutahaean
  */
 public class Course {
 
-    // class definition
+    private String code;
+    private String name;
+    private int credits;
+    private String grade;
 
+    // constructor
+    public Course(String code, String name, int credits, String grade) {
+        this.code = code;
+        this.name = name;
+        this.credits = credits;
+        this.grade = grade;
+    }
+
+    // getter
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
 }

8b7723179bf0729e6bba170cd1d3c75e76fbad06
GriseldaTNHutahaean <iss24026@students.del.ac.id>
2026-03-20 15:34:18 +0700

    Update Student.java

--- src/academic/model/Student.java
+++ src/academic/model/Student.java
@@ -1,11 +1,37 @@
 package academic.model;
 
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S24026 Griselda Hutahaean
  */
 public class Student {
 
-    // class definition
+    private String nim;
+    private String name;
+    private int year;
+    private String studyProgram;
 
+    // constructor
+    public Student(String nim, String name, int year, String studyProgram) {
+        this.nim = nim;
+        this.name = name;
+        this.year = year;
+        this.studyProgram = studyProgram;
+    }
+
+    // getter
+    public String getNim() {
+        return nim;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getStudyProgram() {
+        return studyProgram;
+    }
 }
